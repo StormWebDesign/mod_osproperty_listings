dc13327262fd2ed8e0ad7a4dbcb83fc8157843d9
Russell English <russell.english@stormit.co.uk>
2025-09-03 16:05:42 +0100

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,32 +1,23 @@
 <?php
-
-/**
- * @package     Joomla.Site
- * @subpackage  mod_osproperty_listings
- */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;   // <-- add this
 
 /** @var array $items */
 /** @var Joomla\Registry\Registry $params */
 
 $cols = max(1, (int) $params->get('columns', 3));
-
-/**
- * Map "columns" to your jl- child-width classes
- */
 $childWidth = match ($cols) {
-  1 => 'jl-child-width-1-1',
-  2 => 'jl-child-width-1-2@m',
-  4 => 'jl-child-width-1-4@m',
-  default => 'jl-child-width-1-3@m',
+    1 => 'jl-child-width-1-1',
+    2 => 'jl-child-width-1-2@m',
+    4 => 'jl-child-width-1-4@m',
+    default => 'jl-child-width-1-3@m',
 };
 
 if (empty($items)) : ?>
   <div class="jl-text-meta"><?php echo Text::_('MOD_OSPROPERTY_LISTINGS_NONE'); ?></div>
-<?php return;
+  <?php return;
 endif;
 ?>
 
@@ -39,7 +30,8 @@ endif;
             <img
               src="<?php echo htmlspecialchars($item->image, ENT_QUOTES, 'UTF-8'); ?>"
               alt="<?php echo htmlspecialchars($item->title ?? '', ENT_QUOTES, 'UTF-8'); ?>"
-              jl-cover>
+              jl-cover
+            >
             <canvas width="800" height="600"></canvas>
           </div>
         <?php endif; ?>
@@ -54,20 +46,18 @@ endif;
           <?php if (isset($item->price) && $item->price !== null && $item->price !== '') : ?>
             <div class="jl-text-bold jl-margin-xsmall-bottom">
               <?php
-              $fmt = new \NumberFormatter('en_GB', \NumberFormatter::CURRENCY);
-              // Resolve currency code (fallback GBP)
-              $currencyCode = 'GBP';
-              if (!empty($item->currency_id)) {
-                $currencyCode = \Joomla\Module\OspropertyListings\Site\Helper\PropertiesHelper::resolveCurrencyCode(
-                  \Joomla\CMS\Factory::getContainer()->get(\Joomla\Database\DatabaseInterface::class),
-                  $item->currency_id
-                );
-              }
-              echo $fmt->formatCurrency((float) $item->price, $currencyCode);
+                $fmt = new \NumberFormatter('en_GB', \NumberFormatter::CURRENCY);
+                $currencyCode = 'GBP';
+                if (!empty($item->currency_id)) {
+                    $currencyCode = \Joomla\Module\OspropertyListings\Site\Helper\PropertiesHelper::resolveCurrencyCode(
+                        \Joomla\CMS\Factory::getContainer()->get(\Joomla\Database\DatabaseInterface::class),
+                        $item->currency_id
+                    );
+                }
+                echo $fmt->formatCurrency((float) $item->price, $currencyCode);
               ?>
             </div>
           <?php endif; ?>
-
 
           <?php if (!empty($item->city)) : ?>
             <div class="jl-text-meta jl-margin-xsmall-bottom">
@@ -78,12 +68,21 @@ endif;
 
           <?php if (!empty($item->description)) :
             $short = mb_substr($item->description, 0, 160);
-            if (mb_strlen($item->description) > 160) {
-              $short .= '…';
-            }
+            if (mb_strlen($item->description) > 160) { $short .= '…'; }
           ?>
             <p class="jl-margin-remove-top"><?php echo htmlspecialchars($short, ENT_QUOTES, 'UTF-8'); ?></p>
           <?php endif; ?>
+
+          <?php
+            // Build a safe, non-SEF fallback route to OS Property detail
+            // (Most OS Property installs accept: task=property_details&id=ID)
+            $detailUrl = Route::_('index.php?option=com_osproperty&task=property_details&id=' . (int) $item->id);
+          ?>
+          <div class="jl-margin-small-top">
+            <a class="jl-button jl-button-primary jl-button-small" href="<?php echo $detailUrl; ?>">
+              <?php echo Text::_('JREADMORE'); ?>
+            </a>
+          </div>
         </div>
       </article>
     </div>
