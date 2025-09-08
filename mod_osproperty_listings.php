f33594e4642bd40e3d02dfa8f9ae26512e41f17b
Russell English <russell.english@stormit.co.uk>
2025-09-08 18:07:23 +0100

    Update mod_osproperty_listings.php

--- mod_osproperty_listings.php
+++ mod_osproperty_listings.php
@@ -3,7 +3,6 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
-
 use Joomla\Module\OspropertyListings\Helper\PropertiesHelper;
 
 $items = PropertiesHelper::getItems($params);
