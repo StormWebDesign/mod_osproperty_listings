52d1da6d50a5b357af1152af946d63fe65b1e69e
Russell English <russell.english@stormit.co.uk>
2025-09-08 18:11:47 +0100

    Fixing namespace bug

--- mod_osproperty_listings.php
+++ mod_osproperty_listings.php
@@ -3,6 +3,10 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
+
+// Manually require the helper to avoid PSR-4 mapping issues
+require_once __DIR__ . '/src/Helper/PropertiesHelper.php';
+
 use Joomla\Module\OspropertyListings\Helper\PropertiesHelper;
 
 $items = PropertiesHelper::getItems($params);
