b07b4eb1186fe74a9c18b29da92385cebf793aad
Russell English <russell.english@stormit.co.uk>
2025-09-09 12:29:58 +0100

    Updated UK to JL

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,18 +14,18 @@ $image_location = 'images/osproperty/properties/'; // Base image location
 
 ?>
 
-<div class="uk-grid-small uk-child-width-1-<?php echo $columns; ?>@s uk-grid-match" uk-grid>
+<div class="jl-grid-small jl-child-width-1-<?php echo $columns; ?>@s jl-grid-match" jl-grid>
     <?php foreach ($items as $item): ?>
         <div>
-            <div class="uk-card uk-card-default uk-card-hover uk-card-small">
+            <div class="jl-card jl-card-default jl-card-hover jl-card-small">
                 <?php if ($item->image): ?>
-                    <div class="uk-card-media-top">
+                    <div class="jl-card-media-top">
                         <img src="<?php echo $image_location . $item->id . '/medium/' . $item->image; ?>"
                              alt="<?php echo htmlspecialchars($item->pro_name, ENT_QUOTES, 'UTF-8'); ?>">
                     </div>
                 <?php endif; ?>
-                <div class="uk-card-body">
-                    <h3 class="uk-card-title">
+                <div class="jl-card-body">
+                    <h3 class="jl-card-title">
                         <?php echo htmlspecialchars($item->pro_name, ENT_QUOTES, 'UTF-8'); ?>
                     </h3>
                     <p>
@@ -35,17 +35,13 @@ $image_location = 'images/osproperty/properties/'; // Base image location
                         <?php echo htmlspecialchars(mb_strimwidth($item->pro_small_desc, 0, 60, '...'), ENT_QUOTES, 'UTF-8'); ?>
                     </p>
                     <p>
-                        <span uk-icon="location"></span>
+                        <span jl-icon="location"></span>
                         <?php echo htmlspecialchars($item->city_name, ENT_QUOTES, 'UTF-8'); ?>
                     </p>
                 </div>
-                <div class="uk-card-footer">
-                    <a href="<?php echo Route::_($item->link); ?>" class="uk-button uk-button-text">Read More</a>
+                <div class="jl-card-footer">
+                    <a href="<?php echo Route::_($item->link); ?>" class="jl-button jl-button-text">Read More</a>
                 </div>
-<?php echo $image_location . $item->id . '/medium/' . $item->image; ?>
-                <p><small>Property ID: <?php echo (int) $item->id; ?></small></p>
-
-
             </div>
         </div>
     <?php endforeach; ?>
